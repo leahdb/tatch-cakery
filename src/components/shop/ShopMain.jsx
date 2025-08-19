e48494ae650cdcc9b4ef6fb6305f5f82d63ae8a3
lea <lea.hodeib@anghami.com>
2025-08-19 21:30:06 +0300

    fix build

--- src/components/shop/ShopMain.jsx
+++ src/components/shop/ShopMain.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ShopIntro from "./ShopIntro";
 import ProductsMain from "./ProductsMain";
 import BuildYourCake from "./BuildYourCake";
