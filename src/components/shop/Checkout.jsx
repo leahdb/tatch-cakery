18d8c8a9904fa6875dc058f739905ed715086dd9
lea <lea.hodeib@anghami.com>
2025-08-24 21:04:55 +0300

    checkout form

--- src/components/shop/Checkout.jsx
+++ src/components/shop/Checkout.jsx
@@ -6,20 +6,14 @@ const Checkout = () => {
     <div className="row">
       <div className="col-xl-8 col-lg-8 mb-4">
         <div className="card mb-4 border shadow-0">
-          <div className="p-4 d-flex justify-content-between">
-            <div className="">
-              <h5>Have an account?</h5>
-              <p className="mb-0 text-wrap ">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-            </div>
-            <div className="d-flex align-items-center justify-content-center flex-column flex-md-row">
-              <a href="#" className="btn btn-outline-primary me-0 me-md-2 mb-2 mb-md-0 w-100">Register</a>
-              <a href="#" className="btn btn-primary shadow-0 text-nowrap w-100">Sign in</a>
-            </div>
+          <div className="p-4">
+            <h5 className="card-title mb-3">Contact</h5>
+            <input type="text" id="typeText" placeholder="mobile phone number" className="form-control" />
           </div>
         </div>
         <div className="card shadow-0 border">
           <div className="p-4">
-            <h5 className="card-title mb-3">Guest checkout</h5>
+            <h5 className="card-title mb-3">Delivery</h5>
             <div className="row">
               <div className="col-6 mb-3">
                 <p className="mb-0">First name</p>
