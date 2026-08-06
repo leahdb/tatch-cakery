78ed79b095c711c6bc39606471bf5b4ac8232c58
leahdb <hdeiblea72@gmail.com>
2026-08-06 20:28:42 +0300

    small change

--- src/components/shop/ProductDetails.jsx
+++ src/components/shop/ProductDetails.jsx
@@ -192,9 +192,9 @@ export default function ProductDetails() {
             <span className="fs-6 mt-md-2 fw-bold text-light-brown pb-3">{formatLBP(totalPrice)}</span>
 
             {customizationGroups.length > 0 && (
-              <div className="mb-2 py-3 px-2 border-top">
+              <div className="mb-2 pb-3 px-2">
                 {customizationGroups.map((group) => (
-                  <div className="mb-3" key={group.key}>
+                  <div className="mb-3 border-top pt-3" key={group.key}>
                     <label className="form-label fs-6">{group.label}</label>
                     {group.options.map((opt) => {
                       const checked = group.multiple
