81329d50a861b7bc08b7657449b0d6a8597dd5bc
leahdb <hdeiblea72@gmail.com>
2026-08-13 13:39:18 +0300

    change loading animation

--- src/components/common/LoadingScreen.jsx
+++ src/components/common/LoadingScreen.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import loadingVideo from "../../resources/themes/dashboard-v1/img/loading.mp4";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function LoadingScreen({ className = "d-flex align-items-center justify-content-center" }) {
   return (
     <div className={className} style={{ height: "100vh" }}>
-      <video
-        src={loadingVideo}
-        autoPlay
+      <DotLottieReact
+        src="https://lottie.host/b5f5afc5-d0d1-47ce-b89c-e1bef88e6a76/MPx79OzdoO.lottie"
         loop
-        muted
-        playsInline
-        style={{ height: "auto", maxWidth: "260px" }}
+        autoplay
+        style={{ height: "auto" }}
       />
     </div>
   );
